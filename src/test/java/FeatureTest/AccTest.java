c86d5b3b429b4a33bfc6eb588d6c5314cc0e7321
Asus <saeedenab87@gmail.com>
2025-05-17 19:29:55 +0300

    try

--- src/test/java/FeatureTest/AccTest.java
+++ src/test/java/FeatureTest/AccTest.java
@@ -1,17 +1,21 @@
 package FeatureTest;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features="UserCases",
-        glue = "FeatureTest"
-
+        features="UserCases", // Path to your feature files
+        glue = "FeatureTest", // Package where step definitions are
+        plugin = {
+                "pretty", // Pretty console output
+                "html:target/cucumber-reports.html", // HTML report
+                "json:target/cucumber.json" // JSON report for CI tools
+        },
+        monochrome = true, // Clean console output
+        tags = "@smoke" // Optional: only run tests with @smoke tag
 )
 public class AccTest {
-
-
+        // This class should be empty - it's just a test runner
 }
